99aabccfb1a5429c56568555db2d0e53424b320e
Jakub <149338562+Kubulax@users.noreply.github.com>
2025-02-24 23:41:26 +0100

    Aktualizacja routes
    Dodanie route do tabeli wyników oraz poprawienie nazewnictwa.

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,8 @@ import './index.css'
 import { BrowserRouter, Route, Routes } from 'react-router'
 import { GamePage } from './pages/GamePage.tsx'
 import { TitlePage } from './pages/TitlePage.tsx'
+import { SettingsPage } from './pages/SettingsPage.tsx'
+import { LeaderboardPage } from './pages/LeaderboardPage.tsx'
 import { Game } from 'phaser'
 import { Settings } from './pages/Settings.tsx'
 
@@ -13,7 +15,8 @@ createRoot(document.getElementById('root')!).render(
       <Routes>
         <Route path='/' element={<TitlePage />} />
         <Route path='/game' element={<GamePage />} />
-        <Route path='/settings' element={<Settings />} />
+        <Route path='/settings' element={<SettingsPage />} />
+        <Route path='/leaderboard' element={<LeaderboardPage />} />
       </Routes>
     </BrowserRouter>
   </StrictMode>
